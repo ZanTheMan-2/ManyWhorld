22f93d4dcde4f705480d6b8958791c835c9d6f33
ZanTheMan <vanwyk.zander05@gmail.com>
2023-02-26 17:12:38 +0200

    Player movement complted

--- Manywhords/Assets/!MAIN!/Scripts/pMovement.cs
+++ Manywhords/Assets/!MAIN!/Scripts/pMovement.cs
@@ -4,116 +4,34 @@ using UnityEngine;
 
 public class pMovement : MonoBehaviour
 {
-    [Header("Movement")]
-    public float moveSpeed;
 
-    public float groundDrag;
+    public Rigidbody Rigid;
+    public float MouseSensitivity;
+    public float MoveSpeed;
+    public float JumpForce;
 
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool readyToJump;
+    public bool grounded;
 
-    [HideInInspector] public float walkSpeed;
-    [HideInInspector] public float sprintSpeed;
-
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
-
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
-
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
-    private void Start()
+    void FixedUpdate()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-        readyToJump = true;
-    }
-
-    private void Update()
-    {
-        // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
-
-        MyInput();
-        SpeedControl();
-
-        // handle drag
-        if (grounded)
-            rb.drag = groundDrag;
+        if (Physics.Raycast(transform.position, Vector3.down, 2))
+        {
+            print("grounded");
+            grounded = true;
+        }
         else
-            rb.drag = 0;
-
+            grounded = false;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        MovePlayer();
-    }
+        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
 
-    private void MyInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        // when to jump
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
-        {
-            readyToJump = false;
-
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
+        if (grounded == true) { 
+        if (Input.GetKeyDown("space"))
+            Rigid.AddForce(transform.up * JumpForce);
         }
-    }
-
-    private void MovePlayer()
-    {
-        // calculate movement direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        // on ground
-        if (grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-        // in air
-        else if (!grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-    }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        // limit velocity if needed
-        if (flatVel.magnitude > moveSpeed)
-        {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
-        }
-    }
-
-    private void Jump()
-    {
-        // reset y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-    private void ResetJump()
-    {
-        readyToJump = true;
     }
 }
--- Manywhords/Assets/!MAIN!/Scripts/playerCam.cs
+++ Manywhords/Assets/!MAIN!/Scripts/playerCam.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class playerCam : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mousSens = 100f;
+    public Transform body;
+    public Transform gun1;
+    float xRotation = 0f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mousSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mousSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            mousSens -= 50;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            mousSens += 50;
+        }
+
+
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        body.Rotate(Vector3.up * mouseX);
+
     }
+
 }
